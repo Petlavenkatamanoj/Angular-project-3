09c909565fdce8cac8c949daf1f456d4f4664497
manoj <petlavenkatamanoj@gmail.com>
2025-09-04 13:09:53 +0530

    updated ticket field

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, output  } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms'; 
 
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
   host:{
@@ -15,4 +16,8 @@ import { ControlComponent } from "../../../shared/control/control.component";
 })
 export class NewTicketComponent {
 
+   sendData = output<{ text: string; textarea: string }>();
+  onSubmit(inputData: HTMLInputElement, textData: HTMLTextAreaElement) {
+       this.sendData.emit({text:'inputData.value',textarea:'textData.value'});
+ }
 }
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent,CommonModule],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
 
+  array: { text: string; textarea: string }[] = [];
+
+  addFunction(sendData:{ text: string; textarea: string })
+  {
+    this.array.push(sendData);
+  }
+  
 }
